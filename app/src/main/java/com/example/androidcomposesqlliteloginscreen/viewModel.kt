c73f13c18ad08528dd58b817b4c24bdfe14207b9
chenelias <eliaschen.dev@gmail.com>
2024-12-04 17:37:10 +0800

    Add user Auth and remember sign in status

--- app/src/main/java/com/example/androidcomposesqlliteloginscreen/viewModel.kt
+++ app/src/main/java/com/example/androidcomposesqlliteloginscreen/viewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 
 class userAuth : ViewModel() {
     var starterScreen = mutableStateOf("signin")
+
     fun changeStarterScreen(screen: String) {
         starterScreen.value = screen
     }
